2a3f18dd24ae32ff89ab8966f081d888ecdee480
joshuastewart417 <joshua.stewart780@gmail.com>
2021-06-23 11:24:10 -0500

    added request delete functionality

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -1,22 +1,43 @@
 const applicationState = {};
 
-    const API = "http://localhost:8088"
+const API = "http://localhost:8088";
 
 export const fetchRequests = () => {
-    return fetch(`${API}/requests`)
-        .then(response => response.json())
-        .then(
-            (serviceRequests) => {
-                // Store the external state in application state
-                applicationState.requests = serviceRequests
-            }
-        )
-    }
-
-
-export const getRequests = () => {
-    return database.requests.map((request) => ({...request}));
+  return fetch(`${API}/requests`)
+    .then((response) => response.json())
+    .then((serviceRequests) => {
+      // Store the external state in application state
+      applicationState.requests = serviceRequests;
+    });
 };
 
+export const getRequests = () => {
+  return applicationState.requests.map((request) => ({ ...request }));
+};
+const mainContainer = document.querySelector("#container");
 
+export const sendRequest = (userServiceRequest) => {
+  const fetchOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userServiceRequest),
+   };
 
+   return fetch(`${API}/requests`, fetchOptions)
+      .then(response => response.json())
+      .then(() => {
+        mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+});
+
+};
+
+export const deleteRequest = (id) => {
+  return fetch(`${API}/requests/${id}`, { method: "DELETE" })
+      .then(
+          () => {
+              mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+          }
+      )
+}
